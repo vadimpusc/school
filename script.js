4a387602a072e0d04795ab70667507fdc15ff975
Vadim Pusceddu <hankwodo@gmail.com>
2023-05-20 19:02:56 +0100

    re

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementsByTagName("nav")[0];
+    if (x.className === "") {
+      x.className += "responsive";
+    } else {
+      x.className = "";
+    }
+  }
+  
